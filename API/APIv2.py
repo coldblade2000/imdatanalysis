440609ba3dcd27bd7bf8fd4c1fa621415de65526
greco380 <greco.joshua@gmail.com>
2018-07-05 12:03:15 -0700

    Continuing to work on the KNN. I got the program to read the CSV file. Thx Meg.

--- API/APIv2.py
+++ API/APIv2.py
@@ -2,14 +2,21 @@ import csv
 import random
 
 
-def loadDataset(filename , split, trainingSet=[], testSet=[]):
-    with open('iris.data', 'rb') as csvfile:
+def loaddataset(filename, split, trainingset = [], testset = []):
+    with open(filename, 'r') as csvfile:
         lines = csv.reader(csvfile)
         dataset = list(lines)
         for x in range(len(dataset) - 1):
             for y in range(4):
                 dataset[x][y] = float(dataset[x][y])
             if random.random() < split:
-                trainingSet.append(dataset[x])
+                trainingset.append(dataset[x])
             else:
-                testSet.append(dataset[x])
+                testset.append(dataset[x])
+
+
+trainingSet = []
+testSet = []
+loaddataset('iris.data.csv', 0.66, trainingSet, testSet)
+print('Train: ' + repr(len(trainingSet)))
+print('Test: ' + repr(len(testSet)))
