6674f933439497f6cba60393f48cd8d2794ee172
greco380 <greco.joshua@gmail.com>
2018-07-05 11:16:50 -0700

    Working on the KNN Algorithm

---
+++ API/APIv2.py
@@ -0,0 +1,15 @@
+import csv
+import random
+
+
+def loadDataset(filename , split, trainingSet=[], testSet=[]):
+    with open('iris.data', 'rb') as csvfile:
+        lines = csv.reader(csvfile)
+        dataset = list(lines)
+        for x in range(len(dataset) - 1):
+            for y in range(4):
+                dataset[x][y] = float(dataset[x][y])
+            if random.random() < split:
+                trainingSet.append(dataset[x])
+            else:
+                testSet.append(dataset[x])
